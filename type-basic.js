e0cb95ea9e700fa82918b6e4c91747a2b3df5cd8
unknown <web.faisal.bd@gmail.com>
2022-01-20 16:06:16 +0600

    type basics

---
+++ type-basic.js
@@ -0,0 +1,5 @@
+var myName = "Faisal";
+console.log(myName);
+// myName=40; // error
+var marks = 40;
+console.log(myName + " is my " + marks);
---
+++ type-basic.ts
@@ -0,0 +1,9 @@
+let myName="Faisal";
+console.log(myName);
+
+// myName=40; // error
+
+let marks=40;
+console.log(myName+ " has gotten " +marks);
+
+
